5a41403560d33210c35568fcf0c198f2db8efdd3
Benjamin Winter - Leinweber <Ben@Benjamins-MacBook-Pro.local>
2022-08-17 13:01:35 +0100

    content type set for react views in urls.py in project dir

--- greening_the_spark/urls.py
+++ greening_the_spark/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('greening_the_spark_api.urls')),
     path('baton/', include('baton.urls')),
-    re_path('.*', TemplateView.as_view(template_name="index.html"))
+    re_path('.*', TemplateView.as_view(template_name="index.html", content_type='application/javascript'))
 ]
