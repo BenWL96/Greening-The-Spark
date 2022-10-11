34cb69467f34c414c4b1d0abd072f871710f08c7
Benjamin Winter - Leinweber <Ben@Benjamins-MacBook-Pro.local>
2022-10-11 11:43:50 +0100

    staticfiles now configured, react project styling should now be visible

--- greening_the_spark/settings.py
+++ greening_the_spark/settings.py
@@ -158,11 +158,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-"""STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-"""
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
