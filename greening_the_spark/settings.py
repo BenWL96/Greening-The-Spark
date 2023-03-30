21d557400ee97b53806998082af46cceef37e5d0
Benjamin Winter - Leinweber <Ben@Benjamins-MacBook-Pro.local>
2023-03-30 16:33:13 +0100

    JAWSDB_URL env configured

--- greening_the_spark/settings.py
+++ greening_the_spark/settings.py
@@ -78,7 +78,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'greening_the_spark.wsgi.application'
 
-DATABASES = {
+
+DATABASES['default'] = dj_database_url.config(default=config("JAWSDB_URL", default=''),)
+
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': config("DATABASE_NAME_2", default=''),
@@ -88,7 +92,7 @@ DATABASES = {
         'PASSWORD': config('DATABASE_PASS_2', default=''),
         'OPTIONS': {'sql_mode': 'traditional'}}
 }
-
+"""
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
